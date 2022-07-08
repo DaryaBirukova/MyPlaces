6af920f7571588e3f7e2e754169cea6a4d6da22f
Daria Biriukova <dar.biriukova@gmail.com>
2022-07-08 14:09:45 +0700

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Дарья Бирюкова on 08.07.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
